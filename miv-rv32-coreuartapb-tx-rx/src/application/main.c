3ec094da52278b28234de7325d7d454955565a28
Conor Paxton <conor.paxton@microchip.com>
2022-10-18 16:38:30 +0100

    Squashed 'driver-examples/CoreUARTapb/' changes from 0dd71ff..3152660
    3152660 Merge branch 'develop-2021.10updates' of https://bitbucket.microchip.com/scm/fpga_bu_soft_ip_es/coreuartapb-miv-rv32-examples into develop-2021.10updates
    e0ef093 Fixes review comments
    0fed843 Jenkinsfile: adding CI integration
    f399915 main.c: removes interrupt APIs since the github reference designs dont connect TXRDY/RXRDY interrupt signals
    58b9fb3 hal_irq.c: Adds endofline
    082f5dc hw platform: Removed unsed base addresses. Set clock as per design
    1a55679 Renaming project folder
    2c811b9 Renaming project to with a better name.
    1c8336b adding template example project.
    
    git-subtree-dir: driver-examples/CoreUARTapb
    git-subtree-split: 31526608b7498f672a146e51e84b7fef0dcee2b0

---
+++ miv-rv32-coreuartapb-tx-rx/src/application/main.c
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright 2019-2021 Microchip FPGA Embedded Systems Solutions.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Application demonstrating the use of CoreUARTapb driver. Please see the
+ * README.md for more details.
+ */
+
+#include <drivers/fpga_ip/CoreUARTapb/core_uart_apb.h>
+#include "fpga_design_config/hw_platform.h"
+#include "miv_rv32_hal/miv_rv32_hal.h"
+
+/******************************************************************************
+ * Maximum receiver buffer size.
+ *****************************************************************************/
+#define MAX_RX_DATA_SIZE    256
+
+/******************************************************************************
+ * CoreUARTapb instance data.
+ *****************************************************************************/
+UART_instance_t g_uart;
+
+/******************************************************************************
+ * Instruction message. This message will be transmitted over the UART to
+ * HyperTerminal when the program starts.
+ *****************************************************************************/
+uint8_t g_message[] =
+"\n\r\n\r\n\rThis example demonstrates CoreUARTapb polled transmit and receiver APIs. \
+\n\rAll characters typed will be echoed back.\n\r";
+
+/******************************************************************************
+ * main function.
+ *****************************************************************************/
+void main( void )
+{
+    uint8_t rx_data[MAX_RX_DATA_SIZE];
+    size_t rx_size;
+
+    /**************************************************************************
+     * Initialize CoreUARTapb with its base address, baud value, and line
+     * configuration.
+     *************************************************************************/
+    UART_init( &g_uart, COREUARTAPB0_BASE_ADDR,
+            BAUD_VALUE_115200, (DATA_8_BITS | NO_PARITY) );
+
+    /**************************************************************************
+     * Send the instructions message.
+     *************************************************************************/
+    UART_polled_tx_string( &g_uart, g_message );
+
+    /**************************************************************************
+     * Infinite Loop.
+     *************************************************************************/
+    while(1)
+    {
+        /*************************************************************************
+         * Check for any errors in communication while receiving data
+         ************************************************************************/
+        if(UART_APB_NO_ERROR == UART_get_rx_status(&g_uart))
+        {
+            /**********************************************************************
+             * Read data received by the UART.
+             *********************************************************************/
+            rx_size = UART_get_rx( &g_uart, rx_data, sizeof(rx_data) );
+
+            /**********************************************************************
+             * Echo back data received, if any.
+             *********************************************************************/
+            if ( rx_size > 0 )
+            {
+                UART_send( &g_uart, rx_data, rx_size );
+            }
+        }
+    }
+}
