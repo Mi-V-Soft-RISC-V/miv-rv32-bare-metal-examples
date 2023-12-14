e35453516bf7f38cd9482e2d91e35983c8e18bd5
Lavakrishna Dussa <Lavakrishna.Dussa@microchip.com>
2023-12-14 10:10:25 +0000

    Squashed 'driver-examples/CoreSPI/' changes from 44f4580..c8ea77f
    c8ea77f Update README file
    5628026 Update copyright
    57b436a Update gitignore file
    1f116c2 Update debug configuration settings
    0da91e2 Update project property settings
    8ac1d12 Update fpga_design_config.h file
    19e60d1 Update platform
    9dc6933 Remove README file
    
    git-subtree-dir: driver-examples/CoreSPI
    git-subtree-split: c8ea77f2b5f6e7b15d290ad0d1258e6dddd2898c

--- miv-rv32-corespi-flash/src/boards/polarfire-eval-kit/fpga_design_config/fpga_design_config.h
+++ miv-rv32-corespi-flash/src/boards/polarfire-eval-kit/fpga_design_config/fpga_design_config.h
@@ -1,31 +1,46 @@
 /*******************************************************************************
- * Copyright 2017-2023 Microchip FPGA Embedded Systems Solutions.
+ * Copyright 2022 Microchip FPGA Embedded Systems Solutions.
  *
  * SPDX-License-Identifier: MIT
  *
  * @file fpga_design_config.h
  * @author Microchip FPGA Embedded Systems Solutions
- * @brief This file is taken from the miv_rv32_hal/sample_fpga_design_config.h
+ * @brief Sample design configuration settings.
  *
  */
-
-/*=========================================================================*//**
-  @mainpage Sample file detailing how fpga_design_config.h should be constructed for
-    the Mi-V processors.
+ /*========================================================================*//**
+  @mainpage
+    Example file detailing how the fpga_design_config.h should be constructed
+    for the SoftConsole project targeted for Mi-V processors.
 
     @section intro_sec Introduction
-    The fpga_design_config.h is to be located in the project root directory.
-    Currently this file must be hand crafted when using the Mi-V Soft processor.
+    The SoftConsole project targeted for Mi-V processors now have an improved
+    folder structure. Detailed description of the folder structure is available
+    at https://github.com/Mi-V-Soft-RISC-V/miv-rv32-documentation.
 
-    You can use this file as sample.
-    Rename this file from sample_fpga_design_config.h to fpga_design_config.h and 
-	store it in the root folder of your project. Then customize it per your HW design.
+    The fpga_design_config.h must be stored as shown below
+    <project-root>/boards/<board-name>/fpga_design_config.h
 
-    @section driver_configuration Project configuration Instructions
+    Currently this file must be hand crafted when using the Mi-V Soft Processor.
+    In future, all the design and soft IP configurations will be automatically
+    generated from the Libero design description data.
+    
+    You can use this sample file as an example.
+    Rename this file from sample_fpga_design_config.h to fpga_design_config.h
+    and then customize it per your hardware design.
+
+    @section Project configuration Instructions
     1. Change SYS_CLK_FREQ define to frequency of Mi-V Soft processor clock
-    2  Add all other core BASE addresses
-    3. Add peripheral Core Interrupt to Mi-V Soft processor interrupt mappings
-    4. Define MSCC_STDIO_UART_BASE_ADDR if you want a CoreUARTapb mapped to STDIO
+    2  Add all the soft IP core BASE addresses
+    3. Add the peripheral Core Interrupts to Mi-V Soft processor IRQ number
+       mappings
+    4. Define MSCC_STDIO_UART_BASE_ADDR if you want a CoreUARTapb mapped to
+       STDIO
+
+    **NOTE**
+    In the legacy folder structures, the file hw_config.h as was used at the
+    root of the project folder. This file is now depricated.
+
 *//*=========================================================================*/
 
 #ifndef FPGA_DESIGN_CONFIG_H_
@@ -33,31 +48,37 @@
 
 /***************************************************************************//**
  * Soft-processor clock definition
- * This is the only clock brought over from the Mi-V Soft processor Libero design.
+ * This is the only clock brought over from the Mi-V Libero design.
  */
+#ifndef SYS_CLK_FREQ
 #define SYS_CLK_FREQ                    50000000UL
+#endif
 
 /***************************************************************************//**
- * Non-memory Peripheral base addresses
+ * Peripheral base addresses.
  * Format of define is:
  * <corename>_<instance>_BASE_ADDR
+ * The <instance> field is optional if there is only one instance of the core
+ * in the design
+ * MIV_ESS is an extended peripheral subsystem IP core with peripherals 
+ * connections as defined below.
+ * The system can be further extended by attaching APB peripherals to the 
+ * empty APB slots.
  */
 #define COREUARTAPB0_BASE_ADDR          0x71000000UL
-#define COREGPIO_BASE_ADDR              0x70005000UL
-#define COREGPIO_IN_BASE_ADDR           0x70002000UL
-#define CORETIMER0_BASE_ADDR            0x70003000UL
-#define CORETIMER1_BASE_ADDR            0x70004000UL
-#define COREGPIO_OUT_BASE_ADDR          0x70005000UL
+#define CORETIMER0_BASE_ADDR            0x73000000UL
+#define CORETIMER1_BASE_ADDR            0x74000000UL
+#define COREGPIO_OUT_BASE_ADDR          0x75000000UL
 #define CORESPI_BASE_ADDR               0x76000000UL
-
-
 
 /***************************************************************************//**
  * Peripheral Interrupts are mapped to the corresponding Mi-V Soft processor
- * interrupt from the Libero design.
- * There can be up to 31 external interrupts (IRQ[30:0] pins) on the Mi-V Soft
- * processor. The Mi-V Soft processor external interrupts are defined in the
- * riscv_plic.h
+ * interrupt in the Libero design.
+ *
+ * On the legacy RV32 cores, there can be up to 31 external interrupts (IRQ[30:0]
+ * pins). The legacy RV32 Soft processor external interrupts are defined in the
+   miv_rv32_plic.h
+ * 
  * These are of the form
  * typedef enum
 {
@@ -69,25 +90,45 @@
     .
     External_31_IRQn = 31
 } IRQn_Type;
+ 
+ On the legacy RV32 processors, the PLIC identifies the interrupt and passes it
+ on to the processor core. The interrupt 0 is not used. The pin IRQ[0] should
+ map to External_1_IRQn likewise IRQ[30] should map to External_31_IRQn
 
- The interrupt 0 on RISC-V processor is not used. The pin IRQ[0] should map to
- External_1_IRQn likewise IRQ[30] should map to External_31_IRQn
- * Format of define is:
- * <corename>_<instance>_<core interrupt name>
+e.g
+
+#define TIMER0_IRQn                     External_30_IRQn
+#define TIMER1_IRQn                     External_31_IRQn
+
+ The MIV_RV32 soft processor has up to six optional system interrupts, MSYS_EI[n] 
+ in addition to one EXT_IRQ.
+ The MIV_RV32 does not have an inbuilt PLIC and all the interrupts are directly
+ delivered to the processor core, hence unlike legacy RV32 cores, no interrupt
+ number mapping is necessary on MIV_RV32 core.
  */
 
 #define TIMER0_IRQn                     External_30_IRQn
 #define TIMER1_IRQn                     External_31_IRQn
 
 /****************************************************************************
- * Baud value to achieve a 115200 baud rate with a 83MHz system clock.
+ * Baud value to achieve a 115200 baud rate with system clock defined by
+ * SYS_CLK_FREQ.
  * This value is calculated using the following equation:
  *      BAUD_VALUE = (CLOCK / (16 * BAUD_RATE)) - 1
  *****************************************************************************/
 #define BAUD_VALUE_115200               ((SYS_CLK_FREQ / (16 * 115200)) - 1)
 
+/******************************************************************************
+ * Baud value to achieve a 57600 baud rate with system clock defined by
+ * SYS_CLK_FREQ.
+ * This value is calculated using the following equation:
+ *      BAUD_VALUE = (CLOCK / (16 * BAUD_RATE)) - 1
+ *****************************************************************************/
+ #define BAUD_VALUE_57600                ((SYS_CLK_FREQ / (16 * 57600)) - 1)
+
 /***************************************************************************//**
- * User edit section- Edit sections below if required
+ * Define MSCC_STDIO_THRU_CORE_UART_APB in the project settings if you want the
+ * standard IOs to be redirected to a terminal via UART.
  */
 #ifdef MSCC_STDIO_THRU_CORE_UART_APB
 /*
